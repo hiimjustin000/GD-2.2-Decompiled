e7d06e0a89a517fb9b376a20edc08cc8da8a05c8
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-03-04 15:51:19 -0600

    Update AchievementManager.h

--- GD/code/headers/Managers/AchievementManager.h
+++ GD/code/headers/Managers/AchievementManager.h
@@ -1,11 +1,46 @@
 #include "includes.h"
-
 class AchievementManager: public cocos2d::CCNode
 {
-	cocos2d::CCDictionary* m_pAllAchievements;
-	cocos2d::CCDictionary* m_pAllAchievementsSorted;
-	cocos2d::CCDictionary* m_pAchievementRewardDict;
-	void* dwordC;
-	cocos2d::CCDictionary* m_pReportedAchievements;
-	bool m_bNotifiedAchievement;
-};
+public:
+	cocos2d::CCDictionary* m_platformAchievements;
+	cocos2d::CCDictionary* m_achievements;
+	cocos2d::CCArray* m_allAchievementsSorted;
+	cocos2d::CCArray* m_allAchievements;
+	cocos2d::CCDictionary* m_achievementUnlocks;
+	cocos2d::CCDictionary* m_unAchieved;
+	int m_order;
+	bool m_dontNotify;
+	
+	// virtual ~AchievementManager();
+
+	static AchievementManager* sharedState();
+	const char* achievementForUnlock(int _ID, UnlockType type);
+	void AchievementManager::addAchievement(std::string identifier, std::string title, std::string achievedDescription, std::string unachievedDescription, std::string icon, int limits);
+	void addManualAchievements();
+	bool areAchievementsEarned(cocos2d::CCArray* arr);
+	virtual void checkAchFromUnlock(char const*);
+	void dataLoaded(DS_Dictionary* dsdict);
+	void encodeDataTo(DS_Dictionary* dsdict);
+	void firstSetup();
+	cocos2d::CCDictionary* getAchievementRewardDict();
+	cocos2d::CCDictionary* getAchievementsWithID(char const* ID);
+	cocos2d::CCArray* getAllAchievements();
+	cocos2d::CCArray* getAllAchievementsSorted(bool);
+	bool isAchievementAvailable(std::string achievement);
+	bool isAchievementEarned(const char *achievement)
+	int limitForAchievement(std::string);
+	void notifyAchievement(const char* title,const char* achievedDescription, const char* icon);
+	void notifyAchievementWithID(char const* ID);
+	float percentageForCount(int a, int b);
+	int percentForAchievement(char *achievement);
+	void reportAchievementWithID(char *achievement,int percent,bool levelCompleted);
+	void reportPlatformAchievementWithID(char const* ID, int _report);
+	void resetAchievement(char const* achievementName);
+	void resetAchievements();
+	// virtual...
+	// void setup();
+	void storeAchievementUnlocks();
+
+	virtual bool init();
+}
+
