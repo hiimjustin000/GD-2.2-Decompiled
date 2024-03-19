e4edfb45497da06cb77a25adb36163dee26ad3c9
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-03-19 00:04:34 -0600

    Create InfoLayer.cpp

---
+++ GD/code/src/Layers/InfoLayer.cpp
@@ -0,0 +1,25 @@
+#include "includes.h"
+
+void InfoLayer::updateUserScoreFinished(){
+    if (m_canUpdateScore != false) {
+        m_canUpdateScore = false;
+        GameLevelManager::sharedState()->getLevelComments(
+            getID(), m_page, m_itemCount, 
+            GameManager::sharedState()->getGameVariable("0069"), 
+            m_commentkeytype);
+    }
+}
+
+void InfoLayer::commentUploadFinished(int ID)
+{
+  int _ID;
+  GameManager *GM;
+  
+  if ((this->m_score == NULL) && (ID == getID())) {
+    GameManager::sharedState()->setGameVariable(GM,"0069",false);
+    updateCommentModeButtons();
+    loadPage(0,false);
+    return;
+  }
+  return;
+}
