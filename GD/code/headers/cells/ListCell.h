61a3336a684e95cb83ed403c80109bf6cb7ba2df
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-04-09 16:33:36 -0500

    Update ListCell.h

--- GD/code/headers/cells/ListCell.h
+++ GD/code/headers/cells/ListCell.h
@@ -2,5 +2,14 @@
 
 class ListCell : public TableViewCell
 {
-	cocos2d::ccColor4B m_cLine;
+public:
+	ListCell(char const* name, float width, float height);
+
+	void loadFromObject(cocos2d::CCObject*, int, int, int);
+	void updateBGColor(int color);
+
+	virtual bool init();
+	virtual void draw();
+
+	cocos2d::ccColor4B m_lineColor;
 };
