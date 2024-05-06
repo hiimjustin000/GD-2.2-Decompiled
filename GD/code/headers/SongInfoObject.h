562e44f90e4e5cb9ef316d5b9feaa9282179babe
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-05-05 19:29:28 -0500

    Update SongInfoObject.h

--- GD/code/headers/SongInfoObject.h
+++ GD/code/headers/SongInfoObject.h
@@ -1,18 +1,84 @@
+#ifndef __SONGINFOOBJECT_H__
+#define __SONGINFOOBJECT_H__
+
 #include "includes.h"
+#include <map>
+#include <vector>
+#include <string>
 
 class SongInfoObject : public cocos2d::CCNode
 {
-	int m_nSongID;
-	std::string m_sSongName;
-	std::string m_sArtistName;
-	std::string m_sVideoID;
-	std::string m_sYouTubeChannelURL;
-	std::string m_sSongLink;
-	int m_nArtistID;
-	float m_fSongSize;
-	bool m_bIsUnloaded;
-	bool m_bIsVerified; // scouted on newgrounds
-	bool m_bIsBlocked;
-	int m_nSongPriority;
-	LevelSettingsObject* m_pLevelSettings;
+public:
+	int m_songID;
+	std::string m_songName;	
+	std::string m_artistName;	
+	std::string m_youtubeVideo;	
+	std::string m_youtubeChannel;	
+	std::string m_songUrl;
+	int m_artistID;
+	float m_fileSize;	
+	bool m_UnknownSong;	
+	bool m_isVerified;
+	bool m_isBanned;
+	int m_priority;
+
+	std::map<int, std::vector<std::string>> m_tags;
+	std::string m_tag;
+
+	SongInfoObject():
+		m_songID(0),
+  		m_songName(""),
+  		m_artistName(""),
+  		m_youtubeVideo(""),
+  		m_youtubeChannel(""),
+  		m_songUrl(""),
+  		m_artistID(0),
+  		m_fileSize(0.0),
+  		m_UnknownSong(false),
+  		m_isVerified(false),
+  		m_isBanned(false),
+//   *(undefined4 *)&	field16_0x130 = 0;
+//   *(undefined4 *)&	field_0x134 = 0;
+  		m_priority(-1),
+		m_tag("")
+	{};
+
+
+	static SongInfoObject* create(
+		int songID, 
+		std::string songName, 
+		std::string artistName, 
+		int artistID, 
+		float filesize, 
+		std::string youtubeVideo, 
+		std::string youtubeChannel, 
+		std::string url, 
+		int priority
+	);
+	void addTags(std::string tagData);
+	virtual bool canEncode();
+	static SongInfoObject* create(int songID);
+	static SongInfoObject* create(cocos2d::CCDictionary* dict);
+	static SongInfoObject* createWithCoder(DS_Dictionary* dsdict);
+	bool containsTag(int tagID);
+	
+	void encodeWithCoder(DS_Dictionary* dsdict);
+
+
+
+	bool init(
+		int songID,
+		std::string songName,
+		std::string artistName,
+        int artistID,
+		float filesize,
+		std::string youtubeVideo,
+		std::string youtubeChannel,
+        std::string url,
+		int priority
+	);
+
+
 };
+
+#endif // __SONGINFOOBJECT_H__
